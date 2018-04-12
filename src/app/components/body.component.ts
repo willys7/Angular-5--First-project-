7948f3f93928d595b44cbad497e6284700f5049f
willys7 <wilfuentes0320@gmail.com>
2018-04-11 18:50:40 -0600

    directives *ngFor and *ngIf

--- src/app/components/body.component.ts
+++ src/app/components/body.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: 'body.component.html' 
 })
 export class BodyComponent {
+  frase:any = {
+    message:  "Un gran poder conlleva una gran responsabilidad",
+    author:"Ben Parker"
+  }
 
+  show:boolean = false;
+
+  people:string[] = ["Spider Man","Venon","Octopus"]
 
 }
